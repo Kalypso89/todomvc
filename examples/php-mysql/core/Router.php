12cb6074e67adf3d141481b11d8f05517e14657a
Angela <antunezangela89@gmail.com>
2024-02-04 22:33:14 +0100

    Make Router and Site Controller work to display mocked pages

--- examples/php-mysql/core/Router.php
+++ examples/php-mysql/core/Router.php
@@ -12,12 +12,22 @@ class Router
     {
         $router = $this->getUrl();
 
-        if (file_exists('app/controllers/' . ucfirst($router[0]) . '.php')) {
-            echo 'The file exists';
-        } else {
-            echo 'The file doesn\'t exist';
+        if (file_exists('App/Controllers/' . ucfirst($router[0]) . '.php')) {
+            $this->controller = $router[0];
+            unset($router[0]);
         }
+        $class = "\\App\\Controllers\\" . ucfirst($this->controller);
+        $object = new $class;
+
+        if (isset($router[1]) and method_exists($class, $router[1])) {
+            $this->method = $router[1];
+            unset($router[1]);
+        }
+
+        $this->param = $router ? array_values($router) : [];
+        call_user_func_array([$object, $this->method], $this->param);
     }
+
     public function getUrl()
     {
         $url = explode('/', filter_input(INPUT_GET, 'router', FILTER_SANITIZE_URL));
